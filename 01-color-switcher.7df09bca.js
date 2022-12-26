957bd906b9c4a7af67dedfab904c2ba678aba116
polrmn <114829451+polrmn@users.noreply.github.com>
2022-12-26 19:12:56 +0000

    Deploying to gh-pages from @ polrmn/goit-js-hw-09@51a3c5b6b3dd99e77475f80a32487321035ee0f1 🚀

---
+++ 01-color-switcher.7df09bca.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;function n(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.7df09bca.js.map
