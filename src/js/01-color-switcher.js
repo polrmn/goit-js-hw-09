51a3c5b6b3dd99e77475f80a32487321035ee0f1
polrmn <polrmn@gmail.com>
2022-12-26 21:12:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+stopBtnRef.disabled = true;
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+    timerId = setInterval(changeBodyBackgroundColor, 1000);
+}
+
+function onStopBtnClick() {
+    startBtnRef.disabled = false;
+    stopBtnRef.disabled = true;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
